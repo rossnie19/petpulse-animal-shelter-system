4a44b922562b2af414c6b945fa2153b3e31052ff
pia <pia.louise16@gmail.com>
2026-06-23 19:59:20 +0800

    configured database driver to point to shared live infinityfree mysql server

--- db_connect.php
+++ db_connect.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "sql123.infinityfree.com"; // Put your InfinityFree MySQL Hostname here
-$username = "if0_34567890";             // Put your InfinityFree Username here
-$password = "YourHostingPassword";       // Put your InfinityFree Password here
-$database = "if0_34567890_db_petpulse";  // Put your InfinityFree Database Name here
+$servername = "sql208.infinityfree.com"; // Put your InfinityFree MySQL Hostname here
+$username = "if0_42250354";             // Put your InfinityFree Username here
+$password = "petpulserfkp67";       // Put your InfinityFree Password here
+$database = "if0_42250354_db_petpulse";  // Put your InfinityFree Database Name here
 
 $conn = new mysqli($servername, $username, $password, $database);
 
